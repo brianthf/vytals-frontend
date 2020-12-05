372a86fe69679767dd0f8649998efea230fdc9c6
brianthf <blora616@gmail.com>
2020-12-04 21:17:36 -0500

    add types

---
+++ src/types/index.tsx
@@ -0,0 +1,39 @@
+export interface RegisterConfig {
+  firstName: string;
+  lastName: string;
+  username: string;
+  password: string;
+  email: string;
+  birthdate: Date;
+};
+
+export interface LoginConfig {
+  username: string;
+  password: string;
+};
+
+export interface User {
+  isAuthenticated: boolean;
+  id?: number;
+  username?: string;
+  roles?: Array<string>;
+};
+
+export interface Activity {
+  type: string;
+  description: string;
+  startTime: Date;
+  endTime: Date;
+  duration?: string;
+  user_id?: number;
+};
+
+export interface Reading {
+  weight: number;
+  blood_pressure: number;
+  temperature: number;
+  oxygenLevel: number;
+  pulse: number;
+  timestamp: Date;
+  user_id?: number;
+};
