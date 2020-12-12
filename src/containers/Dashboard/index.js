6bcad5efefca70163eccdcf541130d88febe192f
brianthf <blora616@gmail.com>
2020-12-12 18:49:37 -0500

    profile

--- src/containers/Dashboard/index.js
+++ src/containers/Dashboard/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { Container, Col, Row } from "react-bootstrap";
 import FormTab from "../../components/Forms";
+import { Profile } from "../../components/Profile";
 
 export default function Dashboard() {
   return (
     <Container>
       <Row className="justify-content-md-center">
+        <Col>
+          <Profile />
+        </Col>
         <Col>
           <FormTab />
         </Col>
