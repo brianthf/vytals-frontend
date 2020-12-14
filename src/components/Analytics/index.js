73dc0efb46339219d9445b523fa4305b118e04ac
brianthf <blora616@gmail.com>
2020-12-14 11:44:43 -0500

    tabination

--- src/components/Analytics/index.js
+++ src/components/Analytics/index.js
@@ -1,17 +1,32 @@
 import React, { Suspense } from "react";
-import { Card, Spinner } from "react-bootstrap";
+import { Card, Spinner, Tab, Tabs } from "react-bootstrap";
 import ReadingPlot from "./Reading";
 import ActivityPlot from "./Activity";
 import ErrorBoundary from "../../components/ErrorBoundary";
 
 export default function Analytics({ state }) {
+  const [key, setKey] = React.useState("readings");
+
   return (
     <Card>
       <Card.Header>Analytics</Card.Header>
       <Card.Body>
-        <ErrorBoundary fallback={<h1>Could not fetch data.</h1>}>
-          <Suspense fallback={<Spinner />}></Suspense>
-        </ErrorBoundary>
+        <Tabs id="controlled-tab" activeKey={key} onSelect={(k) => setKey(k)}>
+          <Tab eventKey="readings" title="Readings">
+            <ErrorBoundary fallback={<h1>Could not fetch data.</h1>}>
+              <Suspense fallback={<Spinner />}>
+                <ReadingPlot state={state} />
+              </Suspense>
+            </ErrorBoundary>
+          </Tab>
+          <Tab eventKey="activities" title="Activities">
+            <ErrorBoundary fallback={<h1>Could not fetch data.</h1>}>
+              <Suspense fallback={<Spinner />}>
+                <ActivityPlot state={state} />
+              </Suspense>
+            </ErrorBoundary>
+          </Tab>
+        </Tabs>
       </Card.Body>
     </Card>
   );
