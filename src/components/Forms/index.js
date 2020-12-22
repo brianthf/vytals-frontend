97aebf6140d76726184186e9ba2fd43102002280
brianthf <blora616@gmail.com>
2020-12-21 23:09:00 -0500

    add

--- src/components/Forms/index.js
+++ src/components/Forms/index.js
@@ -1,13 +1,14 @@
 import React from "react";
-import { Tab, Tabs } from "react-bootstrap";
+import { Row, Tab, Tabs } from "react-bootstrap";
 import { ActivityForm, ReadingForm } from "./Input";
+import "./style.css";
 
 export default function FormTab() {
   const [key, setKey] = React.useState("activity");
 
   return (
-    <Tabs id="controlled-tab" activeKey={key} onSelect={(k) => setKey(k)}>
-      <Tab eventKey="activity" title="Home">
+    <Tabs id="controlled-tab" activeKey={key} onSelect={(k) => setKey(k)} fill>
+      <Tab eventKey="activity" title="Activity">
         <ActivityForm />
       </Tab>
       <Tab eventKey="reading" title="Reading">
